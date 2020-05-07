e36b1bf26451bb73e564190771ac5606241a2a07
Julian QG <31473821+JulianQG@users.noreply.github.com>
2020-05-07 15:21:28 +0100

    Sample modified to use my Azure Subscription

--- 1. Desktop app calls Web API/TodoListService/Startup.cs
+++ 1. Desktop app calls Web API/TodoListService/Startup.cs
@@ -7,54 +7,72 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 namespace TodoListService
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
 
-        public IConfiguration Configuration { get; }
+      public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddProtectedWebApi(Configuration);
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddProtectedWebApi(Configuration);
 
-            services.AddControllers();
-        }
+         services.AddControllers();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                // Since IdentityModel version 5.2.1 (or since Microsoft.AspNetCore.Authentication.JwtBearer version 2.2.0),
-                // PII hiding in log files is enabled by default for GDPR concerns.
-                // For debugging/development purposes, one can enable additional detail in exceptions by setting IdentityModelEventSource.ShowPII to true.
-                // Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+         // Register the Swagger generator, defining one or more Swagger documents
+         services.AddSwaggerGen(c =>
+         {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });            
+         });
+      }
 
-            app.UseHttpsRedirection();
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            // Since IdentityModel version 5.2.1 (or since Microsoft.AspNetCore.Authentication.JwtBearer version 2.2.0),
+            // PII hiding in log files is enabled by default for GDPR concerns.
+            // For debugging/development purposes, one can enable additional detail in exceptions by setting IdentityModelEventSource.ShowPII to true.
+            // Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+            app.UseDeveloperExceptionPage();
+         }
+         else
+         {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+         }
+         // Enable middleware to serve generated Swagger as a JSON endpoint.
+         app.UseSwagger();
 
-            app.UseRouting();
+         // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+         app.UseSwaggerUI(c =>
+         {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            // To serve the Swagger UI at the app's root (http://localhost:<port>/), 
+            // set the RoutePrefix property to an empty string:
+            c.RoutePrefix = "";
+         });
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+         app.UseHttpsRedirection();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+         app.UseRouting();
+
+         app.UseAuthentication();
+         app.UseAuthorization();
+
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapControllers();
+         });
+      }
+   }
 }
